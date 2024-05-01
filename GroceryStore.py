a0982a1ad783fd188869e2fa4f6850e3c7eb1e5b
Serdar <serdar.ocal99@gmail.com>
2024-05-01 14:10:48 +0200

    Cart object added

--- GroceryStore.py
+++ GroceryStore.py
@@ -45,3 +45,73 @@ class Kiwi(Fruit):
         super().__init__(weight_avg=100, weight_range=50, price=1, nutrit_val_protein=1.1,
                          nutrit_val_carbohydrates=14.7, nutrit_val_fat=0.5, nutrit_val_water=83.1,
                          nutrit_val_sugar=9.0, nutrit_val_fiber=3.0)
+
+
+class Cart:
+    def __init__(self):
+        self.items = []
+
+    def add(self, item):
+        self.items.append(item)
+
+    def remove(self, item_type):
+        for item in self.items:
+            if item.get_type() == item_type:
+                self.items.remove(item)
+                return item
+        return None
+
+    def clear(self):
+        self.items = []
+
+    def get_total_weight(self):
+        total_weight = 0
+        for item in self.items:
+            total_weight += item.weight
+        return total_weight
+
+    def get_total_nutrition(self):
+        total_nutrition = {
+            "protein": 0,
+            "carbohydrates": 0,
+            "fat": 0,
+            "water": 0,
+            "sugar": 0,
+            "fiber": 0
+        }
+        for item in self.items:
+            total_nutrition["protein"] += item.protein
+            total_nutrition["carbohydrates"] += item.carbohydrates
+            total_nutrition["fat"] += item.fat
+            total_nutrition["water"] += item.water
+            total_nutrition["sugar"] += item.sugar
+            total_nutrition["fiber"] += item.fiber
+        return total_nutrition
+
+    def get_total_price(self):
+        total_price = 0
+        for item in self.items:
+            total_price += item.price
+        return total_price
+
+    def get_fruit_types(self):
+        types = []
+        for item in self.items:
+            item_type = item.get_type()
+            if item_type not in types:
+                types.append(item_type)
+        return types
+
+    def get_formatted_items(self):
+        items = {}
+        for item in self.items:
+            item_type = item.get_type()
+            if item_type not in items:
+                items[item_type] = 1
+            else:
+                items[item_type] += 1
+        formatted_items = ""
+        for item in items:
+            formatted_items += str(items[item]) + "x" + item + ", "
+        return formatted_items
+
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ To-do List:
 
 - [x] Fruit object will be created
 - [X] 3 new fruit objects will be created
-- [ ] Basket object will be created
+- [X] Basket object will be created
 - [ ] Store object will be created
 - [ ] User object will be created
 - [ ] CLI will be created
